596478c5c6efa8e67e0e496291f8f583c1358f34
Oliver Payne <oliver.payne@lifeway.com>
2024-04-26 21:37:30 -0500

    basic map

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+mod map;
+mod components;
+mod player;
+mod rect;
+use rect::Rect;
+
 use std::cmp::{max, min};
 use bracket_lib::color::{BLACK, RED, YELLOW};
 use bracket_lib::prelude::{BError, BTerm, BTermBuilder, FontCharType, GameState, main_loop, RGB, to_cp437, VirtualKeyCode};
 use bracket_lib::random::RandomNumberGenerator;
 use specs::prelude::*;
 use specs_derive::Component;
+use crate::map::{draw_map, new_map_rooms_and_corridors};
 
 #[derive(Component)]
 struct Position {
@@ -39,6 +46,16 @@ impl<'a> System<'a> for LeftWalker {
     }
 }
 
+#[derive(PartialEq, Copy, Clone)]
+enum TileType {
+    Wall, Floor
+}
+
+pub fn xy_idx(x: i32, y: i32) -> usize {
+    (y as usize * 80) + x as usize
+}
+
+
 struct State {
     ecs: World
 }
@@ -57,6 +74,9 @@ impl GameState for State {
         ctx.print(70, 0, ctx.fps);
         self.run_systems();
         player_input(self, ctx);
+        let map = self.ecs.fetch::<Vec<TileType>>();
+        draw_map(&map, ctx);
+
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
 
@@ -75,13 +95,16 @@ fn main() -> BError {
     state.ecs.register::<Renderable>();
     state.ecs.register::<LeftMover>();
     state.ecs.register::<Player>();
+    let (rooms, map) = new_map_rooms_and_corridors();
+    state.ecs.insert(map);
+    let (player_x, player_y) = rooms[0].center();
     let mut bterm = BTermBuilder::simple80x50()
         .with_title("Rusty Roguelike V2")
         .with_tile_dimensions(12, 12)
         .build()?;
     state.ecs
         .create_entity()
-        .with(Position{x: 40, y:25})
+        .with(Position{x: player_x, y: player_y})
         .with(
             Renderable{
                 glyph: to_cp437('@'),
@@ -115,10 +138,14 @@ fn main() -> BError {
 fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
+    let map = ecs.fetch::<Vec<TileType>>();
 
     for (_player, pos) in (&mut players, &mut positions).join() {
-        pos.x = min(79, max(0, pos.x + delta_x));
-        pos.y = min(49, max(0, pos.y + delta_y));
+        let destination_idx = xy_idx(pos.x + delta_x, pos.y +delta_y);
+        if map[destination_idx] != TileType::Wall {
+            pos.x = min(79, max(0, pos.x + delta_x));
+            pos.y = min(49, max(0, pos.y + delta_y));
+        }
     }
 }
 
@@ -126,10 +153,17 @@ fn player_input(gs: &mut State, ctx: &mut BTerm) {
     match ctx.key {
         None => {}
         Some(key) => match key {
-            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
-            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
-            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
-            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            VirtualKeyCode::Left |
+            VirtualKeyCode::H => try_move_player(-1, 0, &mut gs.ecs),
+
+            VirtualKeyCode::Right |
+            VirtualKeyCode::L => try_move_player(1, 0, &mut gs.ecs),
+
+            VirtualKeyCode::Up |
+            VirtualKeyCode::K => try_move_player(0, -1, &mut gs.ecs),
+
+            VirtualKeyCode::Down |
+            VirtualKeyCode::J => try_move_player(0, 1, &mut gs.ecs),
             _ => {}
         }
     }
